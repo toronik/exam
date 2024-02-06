857ef6313fc2c817ccee75441e01a909949897ca
toronik <toronik@users.noreply.github.com>
2024-02-06 12:08:41 +0000

    toronik build 14 pushed to gh-pages

---
+++ ext/codemirror/enable-codemirror.js
@@ -0,0 +1,95 @@
+function enableCM() {
+    enableCodeMirror('.json:not(.failure)', {name: 'javascript', json: true});
+    enableCodeMirrorMerge('.json.failure', {name: 'javascript', json: true});
+    enableCodeMirror('.xml:not(.failure)', 'application/xml');
+    enableCodeMirrorMerge('.xml.failure', 'application/xml');
+    enableCodeMirrorWithTheme('.htmlmixed.darcula', 'text/html', 'darcula');
+    enableCodeMirrorWithTheme('.handlebars.darcula', {name: "handlebars", base: "text/html"}, 'darcula');
+    enableCodeMirror('.htmlmixed:not(.failure) .htmlmixed:not(.darcula)', 'text/html');
+    enableCodeMirrorMerge('.htmlmixed.failure', 'text/html');
+    enableCodeMirrorHttp('.http:not(.failure)');
+    enableCodeMirrorMerge('.http.failure');
+    document.querySelectorAll('.http, .text, .json, .xml, .htmlmixed').forEach(function (el) {
+        el.style.visibility = "visible";
+    });
+    document.querySelectorAll('.default-collapsed').forEach(function (el) {
+        el.click();
+    });
+    window.dispatchEvent(new Event('resize'));
+}
+
+function unescape(input) {
+    const e = document.createElement('div');
+    e.innerHTML = input;
+    return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+}
+
+function enableCodeMirrorHttp(selector) {
+    let editor, jsons = document.querySelectorAll(selector);
+    for (let i = 0; i < jsons.length; i++) {
+        editor = CodeMirror.fromTextArea(jsons[i], {
+            mode: 'message/http',
+            readOnly: true,
+            scrollbarStyle: "simple",
+            viewportMargin: Infinity
+        });
+    }
+}
+
+function enableCodeMirrorWithTheme(selector, mode, theme) {
+    let value, editor, jsons = document.querySelectorAll(selector);
+
+    for (let i = 0; i < jsons.length; i++) {
+        const el = jsons[i];
+        value = unescape(el.innerHTML);
+        if( el.childNodes.length === 1 ) el.innerHTML = "";
+
+        editor = CodeMirror(el, {
+            lineNumbers: (el.getAttribute("lineNumbers") === 'true'),
+            mode: mode,
+            value: value,
+            readOnly: true,
+            scrollbarStyle: "simple",
+            viewportMargin: Infinity,
+            theme: theme
+        });
+    }
+}
+
+function enableCodeMirror(selector, mode) {
+    enableCodeMirrorWithTheme(selector, mode, "default");
+}
+
+function enableCodeMirrorMerge(selector, mode) {
+    var view, jsons = document.querySelectorAll(selector);
+    for (var i = 0; i < jsons.length; i++) {
+        var target = jsons[i];
+        var expectedValue, actualValue,
+            expected = target.querySelector('.expected'),
+            actual = target.querySelector('.actual');
+
+        if (expected && actual) {
+          expectedValue = unescape(expected.innerHTML);
+          actualValue = unescape(actual.innerHTML);
+
+          actual.parentNode.removeChild(actual);
+          expected.parentNode.removeChild(expected);
+          target.innerHTML = '';
+
+          view = CodeMirror.MergeView(target, {
+            value: expectedValue,
+            readOnly: true,
+            origLeft: null,
+            orig: actualValue,
+            lineNumbers: false,
+            mode: mode,
+            connect: true,
+            highlightDifferences: true,
+            collapseIdentical: true,
+            scrollbarStyle: "simple",
+            viewportMargin: Infinity
+            //theme: "idea"
+          });
+        }
+    }
+}
